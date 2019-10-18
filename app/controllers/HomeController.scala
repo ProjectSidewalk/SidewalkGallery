3bd8aa66c7d3f7f6a560baf51907a273e8d22b7f
Aileen Zeng <aileenz@uw.edu>
2019-10-17 21:22:52 -0700

    First commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import javax.inject._
+
+import play.api.libs.json.Json
+import play.api.mvc._
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def appSummary = Action {
+    Ok(Json.obj("content" -> "Scala Play Angular Seed"))
+  }
+
+  def postTest = Action {
+    print("success!")
+    Ok(Json.obj("content" -> "Post Request Test => Data Sending Success"))
+  }
+}
