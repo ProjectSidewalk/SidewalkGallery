c7ffd542e0fe197bd8c9280c02de71c70aacd3cd
Aileen Zeng <aileenz@uw.edu>
2019-12-12 12:43:09 -0800

    Add end-to-end function

--- app/models/labels/LabelTable.scala
+++ app/models/labels/LabelTable.scala
@@ -10,28 +10,28 @@ case class Label(description: String,
                  labelTypeId: Int,
                  gsvPanoramaId: String,
                  heading: Float,
-                 lat: Float,
-                 lng: Float,
+                 // lat: Float,
+                 // lng: Float,
                  pitch: Float,
                  zoom: Int)
 
-class LabelTable (tag: slick.lifted.Tag) extends Table[Label](tag, Some("sidewalk"), "label") {
+class LabelTable (tag: slick.lifted.Tag) extends Table[Label](tag, "label") {
   def labelId= column[Int]("label_id", O.PrimaryKey, O.AutoInc)
   def gsvPanoramaId = column[String]("gsv_panorama_id")
   def description = column[String]("description")
   def labelTypeId = column[Int]("label_type_id")
   def heading = column[Float]("heading")
   def pitch = column[Float]("pitch")
-  def lat = column[Float]("lat")
-  def lng = column[Float]("lng")
+  // def lat = column[Float]("lat")
+  // def lng = column[Float]("lng")
   def zoom = column[Int]("zoom")
 
   override def * = (description, labelId, labelTypeId,
-  gsvPanoramaId, heading, lat, lng, pitch, zoom) <> ((Label.apply _).tupled, Label.unapply)
+  gsvPanoramaId, heading, /* lat, lng,*/ pitch, zoom) <> ((Label.apply _).tupled, Label.unapply)
 }
 
 object LabelQuery extends TableQuery(new LabelTable(_)) {
-  val db = Database.forConfig("slick.dbs.default")
+  val db = Database.forConfig("slick.dbs.default.db")
 
 
   def getLabels(labelTypeId: Int, count: Int): Future[Seq[Label]] = {
