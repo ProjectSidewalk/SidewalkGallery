89ae1a3a52a32b8326b7198476a532262a637eec
Aileen Zeng <aileenz@uw.edu>
2019-12-13 11:19:43 -0800

    Move subscriptions into private helper

--- ui/src/app/gallery/gallery-root.ts
+++ ui/src/app/gallery/gallery-root.ts
@@ -39,41 +39,40 @@ export class GalleryRoot {
     }
 
     ngOnInit() {
-        console.log("[gallery-root] initializing");
-
-      this.curbRampSubscription = this.galleryService.getCurbRamps().subscribe(
-        result => {
-          this.curbCards = result.map(x => this.jsonToCards(x))
-        });
-
-      this.missingCurbRampSubscription = this.galleryService.getMissingCurbRamps().subscribe(
-        result => {
-          this.missingCurbCards = result.map(x => this.jsonToCards(x))
-        });
-
-      this.obstacleSubscription = this.galleryService.getObstacles().subscribe(
-        result => {
-          this.obstacleCards = result.map(x => this.jsonToCards(x))
-        });
-
-      this.sfcpSubscription = this.galleryService.getSurfaceProblems().subscribe(
-        result => {
-          this.surfaceProbCards = result.map(x => this.jsonToCards(x))
-        });
-
-
-      this.noSidewalkSubscription = this.galleryService.getNoSidewalk().subscribe(
-        result => {
-          this.noSidewalkCards = result.map(x => this.jsonToCards(x))
-        });
+      console.log("[gallery-root] initializing");
+      this.addSubscriptions();
 
   }
 
-    /**
-     * This method is used to test the post request
-     */
-    public postData(): void {
-        console.log("[gallery-root] Testing curb ramp request");
-        this.galleryService.getLabelMetadata(Constants.curbRampId, 10, false);
-    }
+  /**
+   * Adds a subscription for each type of label.
+   */
+  private addSubscriptions(): void {
+    this.curbRampSubscription = this.galleryService.getCurbRamps().subscribe(
+      result => {
+        this.curbCards = result.map(x => this.jsonToCards(x))
+      });
+
+    this.missingCurbRampSubscription = this.galleryService.getMissingCurbRamps().subscribe(
+      result => {
+        this.missingCurbCards = result.map(x => this.jsonToCards(x))
+      });
+
+    this.obstacleSubscription = this.galleryService.getObstacles().subscribe(
+      result => {
+        this.obstacleCards = result.map(x => this.jsonToCards(x))
+      });
+
+    this.sfcpSubscription = this.galleryService.getSurfaceProblems().subscribe(
+      result => {
+        this.surfaceProbCards = result.map(x => this.jsonToCards(x))
+      });
+
+
+    this.noSidewalkSubscription = this.galleryService.getNoSidewalk().subscribe(
+      result => {
+        this.noSidewalkCards = result.map(x => this.jsonToCards(x))
+      });
+
+  }
 }
