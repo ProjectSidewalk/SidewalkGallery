b8ee3decd54154247e1a48ac0a3c6ed9a3699909
Aileen Zeng <aileenz@uw.edu>
2020-01-07 15:43:21 -0800

    Add working implementation of filters

--- ui/src/app/gallery/gallery-overview.ts
+++ ui/src/app/gallery/gallery-overview.ts
@@ -22,6 +22,9 @@ export class GalleryOverview {
   // clicks some tag on the top of the gallery menu to filter out by.
   displayCards: Set<Card> = new Set<Card>();
 
+  // Filters that have been applied on this gallery overview.
+  filters: Set<number> = new Set<number>();
+
   // The title of this gallery. (Should be the name of the label that this gallery overview is
   // representing.
   @Input() title: string = '';
@@ -30,9 +33,6 @@ export class GalleryOverview {
   @Input() tags: Tag[] = [];
 
   ngOnChanges() {
-    console.log("View loaded!");
-    console.log("Cards");
-    console.log(this.cards);
     for (let card of this.cards) {
       this.displayCards.add(card);
     }
@@ -44,8 +44,14 @@ export class GalleryOverview {
    */
   private filterCards(event: TagEvent) {
     if (event.selected) {
+      this.filters.add(event.tagId);
+      console.log("Filters");
+      console.log(this.filters);
       this.removeCards(event.tagId);
     } else {
+      this.filters.delete(event.tagId);
+      console.log("Filters");
+      console.log(this.filters);
       this.addCards(event.tagId);
     }
   }
@@ -55,23 +61,39 @@ export class GalleryOverview {
    * @param tagId
    */
   private removeCards(tagId: number) {
-    console.log("removing cards");
-    console.log(this.displayCards);
     for (let card of this.displayCards.values()) {
-      console.log(card);
       if (!card.tags.has(tagId)) {
         this.displayCards.delete(card);
       }
     }
-    console.log(this.displayCards);
-
   }
 
+  /**
+   * Adds cards to the
+   * @param tagId
+   */
   private addCards(tagId: number) {
     console.log("adding cards");
+    // Check all cards to see if there are new cards that need to be added back into the
+    // display after unapplying a filter.
     for (let card of this.cards) {
+      // If the card is not in the display cards, but does have the tag id, then check
+      // to see if we should add it back in.
       if (!card.tags.has(tagId) && !this.displayCards.has(card)) {
-        this.displayCards.add(card);
+        console.log("Considering a new card to be added back in");
+        console.log(card);
+        let addCard: boolean = true;
+
+        // Only add in tags that have no common filters with the current filters that have
+        // been applied.
+        for (let tag of this.filters) {
+          if (!card.tags.has(tag)) {
+            addCard = false;
+          }
+        }
+        if (addCard) {
+          this.displayCards.add(card);
+        }
       }
     }
     console.log(this.displayCards);
