c75674eee363b5fa624d2b482bec3bdbc8091238
Aileen Zeng <aileenz@uw.edu>
2020-01-27 11:00:51 -0800

    Add more backend code for core sidewalk

---
+++ app/models/core/LabelPointTable.scala
@@ -0,0 +1,42 @@
+package models.core
+
+import models.utils.MyPostgresProfile.api._
+
+case class LabelPoint(alphaX: Double,
+                      alphaY: Double,
+                      canvasHeight: Int,
+                      canvasWidth: Int,
+                      canvasX: Int,
+                      canvasY: Int,
+                      heading: Double,
+                      labelId: Int,
+                      labelPointId: Int,
+                      lat: Double,
+                      lng: Double,
+                      pitch: Double,
+                      svImageX: Int,
+                      svImageY: Int,
+                      zoom: Int)
+
+class LabelPointTable (tag: slick.lifted.Tag) extends Table[LabelPoint](tag, Some("sidewalk"), "label_point"){
+  def alphaX: Rep[Double] = column[Double]("alpha_x")
+  def alphaY: Rep[Double] = column[Double]("alpha_y")
+  def canvasHeight: Rep[Int]  = column[Int]("canvas_height")
+  def canvasWidth: Rep[Int] = column[Int]("canvas_width")
+  def canvasX: Rep[Int] = column[Int]("canvas_x")
+  def canvasY: Rep[Int] = column[Int]("canvas_y")
+  def heading: Rep[Double] = column[Double]("heading")
+  def labelId: Rep[Int] = column[Int]("label_id")
+  def labelPointId: Rep[Int] = column[Int]("label_point_id")
+  def lat: Rep[Double] = column[Double]("lat")
+  def lng: Rep[Double] = column[Double]("lng")
+  def pitch: Rep[Double] = column[Double]("pitch")
+  def svImageX: Rep[Int] = column[Int]("sv_image_x")
+  def svImageY: Rep[Int] = column[Int]("sv_image_y")
+  def zoom: Rep[Int] = column[Int]("zoom")
+
+  // Type: ProvenShape[LabelPoint]
+  override def * = (alphaX, alphaY, canvasHeight, canvasWidth, canvasX, canvasY, heading, labelId,
+    labelPointId, lat, lng, pitch, svImageX, svImageY, zoom) <> (
+    (LabelPoint.apply _).tupled, LabelPoint.unapply)
+}
--- app/models/core/LabelTable.scala
+++ app/models/core/LabelTable.scala
@@ -1,10 +1,43 @@
 package models.core
 
-// TODO(aileenzeng): Populate this with equivalent queries from labels/LabelTable
-// to retrieve labels from core sidewalk.
+import models.labels.LabelTable
+import models.utils.MyPostgresProfile.api._
+import play.api.libs.json.{JsObject, Json}
+
+import scala.concurrent.Future
+import scala.io.Source
+
+case class GalleryLabel(gsvPanoramaId: String,
+                        labelId: Int,
+                        labelTypeId: Int)
+
+/**
+ * Backend model object for a label object.
+ * @param gsvPanoramaId GSV Panorama ID of the label.
+ * @param labelId       Unique ID that represents this label. Used as a primary key in the primary
+ *                      Sidewalk database and can be used to access information in related tables.
+ * @param labelTypeId   Label type of this label [1-7].
+ */
+case class Label(gsvPanoramaId: String,
+                 labelId: Int,
+                 labelTypeId: Int)
+
 //
 // NOTE: Several of these functions will be much more complicated because there
 // will be several joins required to fetch all the data.
-class LabelTable {
+class LabelTable (tag: slick.lifted.Tag) extends Table[Label](tag, Some("sidewalk"), "label") {
+  def labelId: Rep[Int]= column[Int]("label_id", O.PrimaryKey, O.AutoInc)
+  def gsvPanoramaId: Rep[String] = column[String]("gsv_panorama_id")
+  def labelTypeId: Rep[Int] = column[Int]("label_type_id")
 
+  override def * = (gsvPanoramaId, labelId, labelTypeId) <> ((Label.apply _).tupled, Label.unapply)
+}
+
+object LabelQuery extends TableQuery(new LabelTable(_)) {
+  val db = Database.forConfig("slick.dbs.default.db")
+  val labels = TableQuery[LabelTable]
+
+  // TODO(aileenzeng): Define this method, change clients to use GalleryLabels instead of Labels.
+  // def getLabels(labelTypeId: Int, count: Int): Future[Seq[GalleryLabel]] = {
+  // }
 }
