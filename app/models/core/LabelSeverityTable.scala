60716dfdf204b3af9a6004c6ee2081df7c60d28b
Aileen Zeng <aileenz@uw.edu>
2020-02-01 13:03:35 -0800

    Fix naming for backend queries

--- app/models/core/LabelSeverityTable.scala
+++ app/models/core/LabelSeverityTable.scala
@@ -6,7 +6,7 @@ case class LabelSeverity(labelId: Int,
                          labelSeverityId: Int,
                          labelSeverity: Int)
 
-class LabelSeverityTable (tag: slick.lifted.Tag) extends Table[LabelPoint](tag, Some("sidewalk"), "label_point"){
+class LabelSeverityTable (tag: slick.lifted.Tag) extends Table[LabelSeverity](tag, Some("sidewalk"), "label_severity"){
   def labelId: Rep[Int] = column[Int]("label_id")
   def labelSeverityId: Rep[Int] = column[Int]("label_severity_id")
   def labelSeverity: Rep[Int] = column[Int]("label_severity")
--- app/models/core/LabelTypeTable.scala
+++ app/models/core/LabelTypeTable.scala
@@ -6,11 +6,11 @@ case class LabelType(description: String,
                      labelType: Int,
                      labelTypeId: Int)
 
-class LabelTypeTable(tag: slick.lifted.Tag) extends Table[LabelTypeTable](tag, Some("sidewalk"), "label_tag") {
-  def description: Rep[Int] = column[Int]("description")
+class LabelTypeTable(tag: slick.lifted.Tag) extends Table[LabelType](tag, Some("sidewalk"), "label_type") {
+  def description: Rep[String] = column[String]("description")
   def labelType: Rep[Int] = column[Int]("label_type")
   def labelTypeId: Rep[Int] = column[Int]("label_type_id")
 
-  // Type: ProvenShape[LabelTag]
-  override def * = (description, labelType, labelTypeId) <> ((LabelType.apply _).tupled, LabelType.unapply)
+  // Type: ProvenShape[LabelType]
+  override def * =(description, labelType, labelTypeId) <> ((LabelType.apply _).tupled, LabelType.unapply)
 }
